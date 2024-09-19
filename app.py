e7a79e778c3e719bd72cbc7b7e4f15193065d051
adam-exe <53061615+adam-exe@users.noreply.github.com>
2024-09-19 11:28:12 +0200

    Update app.py

--- app.py
+++ app.py
@@ -210,16 +210,6 @@ st.image("images/banner2.png", use_column_width=True)
 with st.sidebar:
     st.image("images/bot.png", width=100)
     st.markdown("""**Hello! I'm Globot, your friendly travel assistant for Olympic Games information. Ask me about travel destinations, weather, and more.**
- 
-**Example Requests:**
-- What is the best time to visit Paris?               
-- Tell me about the Olympic games in Sydney.
-- How many medalists does Croatia have?
-- What are the top tourist attractions in Tokyo?
-- Get the names of all the current Olympic champions in the weightlifting events.
-- Give top 3 countries with biggest increase in tourism for the next year
-- Predict tourism growth of croatia for 2025.
-- Save it to PDF
     """)
 # Images for user and bot
 user_image = "images/user.png"
